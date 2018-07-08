f41cd25a403848c0ac67b24aaad1c7ec6a2f93fd
fractalleaf <sfrimann@gmail.com>
2018-07-08 16:26:04 +0200

    Modified conf files

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,34 +2,56 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Søren Frimann'
-SITENAME = 'The Data Cave'
-SITEURL = ''
+# Site set-up
+MARKUP = ('md', 'ipynb') # markup languages to be used
 
-PATH = 'content'
+THEME = 'themes/Flex'
 
+# PLUGINS
+PLUGIN_PATHS = ['./plugins', './pelican-plugins']
+PLUGINS = ['ipynb.markup', 'render_math', 'representative_image', 'related_posts']
+TYPOGRIFY = True
+
+# PATHS
+PATH = 'content' # path to content directory
+STATIC_PATHS = ['Algorithms/images']
+
+# LANGUAGE AND TIMEZONE
 TIMEZONE = 'Europe/Paris'
-
 DEFAULT_LANG = 'en'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+# SITE METADATA
+AUTHOR = 'Søren Frimann'
+SITENAME = 'Digital Ramblings'
+SITETITLE = 'Digital Ramblings'
+SITESUBTITLE = 'Post Hoc, Ergo Propter Hoc'
+#SITEDESCRIPTION = 'Foo Bar\'s Thoughts and Writings'
+SITEURL = ''
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# MENU STRUCTURE
+MAIN_MENU = True
+# LINKS = (('About', '{filename}/pages/about.md'),)
+SOCIAL = (('linkedin', 'https://www.linkedin.com/in/sfrimann/'),
+          ('github', 'https://github.com/fractalleaf'))
+MENUITEMS = (('Archives', '/archives.html'),
+             ('Categories', '/categories.html'),
+             ('Tags', '/tags.html'),)
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# SYNTAX HIGHLIGHTING
+PYGMENTS_STYLE = 'native'
 
+# ARTICLE MANAGEMENT
+USE_FOLDER_AS_CATEGORY = True
 DEFAULT_PAGINATION = 10
+DEFAULT_CATEGORY = 'Misc'
+
+# LICENSE
+CC_LICENSE = {
+    'name': 'Creative Commons Attribution-ShareAlike',
+    'version': '4.0',
+    'slug': 'by-sa'
+}
+COPYRIGHT_YEAR = 2018
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
